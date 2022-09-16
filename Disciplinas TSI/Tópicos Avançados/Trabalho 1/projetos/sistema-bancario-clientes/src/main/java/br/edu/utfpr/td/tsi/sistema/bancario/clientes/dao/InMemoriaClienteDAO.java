3b736011df8443f670d58f3abaf86ed7af79ac98
Marcos <marcossouzatol@gmail.com>
2022-09-15 21:10:59 -0300

    Subindo trabalho

---
+++ Disciplinas TSI/Tópicos Avançados/Trabalho 1/projetos/sistema-bancario-clientes/src/main/java/br/edu/utfpr/td/tsi/sistema/bancario/clientes/dao/InMemoriaClienteDAO.java
@@ -0,0 +1,43 @@
+package br.edu.utfpr.td.tsi.sistema.bancario.clientes.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.utfpr.td.tsi.sistema.bancario.clientes.dominio.Cliente;
+
+public class InMemoriaClienteDAO implements ClienteDAO{
+	
+	List<Cliente> clientesCadastrados = new ArrayList<Cliente>();
+
+	@Override
+	public void gravar(Cliente cliente) {
+		this.clientesCadastrados.add(cliente);
+		System.out.println("Cliente persistido em memoria.");
+	}
+
+	@Override
+	public Cliente ler(String cpf) {
+		for(Cliente c: clientesCadastrados) {
+			if (c.getCpf().equals(cpf)){
+				System.out.println(c);
+				return c;
+			}else {
+				System.out.println("Cliente não encontrado");
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void remover(String cpf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Cliente> lerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
---
+++ Disciplinas TSI/Tópicos Avançados/Trabalho 1/projetos/sistema-bancario-clientes/src/main/java/br/edu/utfpr/td/tsi/sistema/bancario/clientes/dominio/Cliente.java
@@ -0,0 +1,67 @@
+package br.edu.utfpr.td.tsi.sistema.bancario.clientes.dominio;
+
+public class Cliente {
+
+	private String id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String dataNascimento;
+	private double rendaAnual;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public double getRendaAnual() {
+		return rendaAnual;
+	}
+
+	public void setRendaAnual(double rendaAnual) {
+		this.rendaAnual = rendaAnual;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", email=" + email + ", dataNascimento="
+				+ dataNascimento + ", rendaAnual=" + rendaAnual + "]";
+	}
+
+	
+}
